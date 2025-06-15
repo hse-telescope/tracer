6005210625fa546caee1403f414ab5d8baa165de
olegdayo <ol-sidorenkov@mail.ru>
2025-06-15 22:28:21 +0300

    traces

--- middleware.go
+++ middleware.go
@@ -5,13 +5,17 @@ import (
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/trace"
 )
 
 func AddTracingMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			ctx, span := tracer.Start(r.Context(), r.URL.Path)
+			p := propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{})
+			ctx := p.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+
+			ctx, span := tracer.Start(ctx, r.URL.Path)
 			span.AddEvent("got request", trace.WithAttributes(attribute.KeyValue{
 				Key:   attribute.Key("url"),
 				Value: attribute.StringValue(r.URL.Path),
