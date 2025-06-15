89894da8846719b0e01be4cf45a6ce700d05b579
olegdayo <ol-sidorenkov@mail.ru>
2025-06-15 20:39:16 +0300

    fixes

--- middleware.go
+++ middleware.go
@@ -1,11 +1,22 @@
 package tracer
 
-import "net/http"
+import (
+	"net/http"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
 
 func AddTracingMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			tracer.Start(r.Context(), r.URL.Path)
+			ctx, span := tracer.Start(r.Context(), r.URL.Path)
+			span.AddEvent("got request", trace.WithAttributes(attribute.KeyValue{
+				Key:   attribute.Key("url"),
+				Value: attribute.StringValue(r.URL.Path),
+			}))
+			defer span.End()
+			r = r.WithContext(ctx)
 			handler.ServeHTTP(w, r)
 		},
 	)
